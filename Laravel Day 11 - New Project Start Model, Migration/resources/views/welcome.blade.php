dad923196c7581e40a4b241c12900958e4006dac
Dhan Raj Gurung <drgurung82@gmail.com>
2026-08-01 14:19:20 +0545

    Initial commit

--- Laravel Day 11 - New Project Start Model, Migration/resources/views/welcome.blade.php
+++ Laravel Day 11 - New Project Start Model, Migration/resources/views/welcome.blade.php
@@ -1,12 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="https://cdnjsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.3.0/css/all.min.css"
+        integrity="sha512-ApSLB1Pd3/bZN8fWB/RG9YhN/7bd9Hkf3AGaE2mPfebjrxagjuBtx2GcgdqIlJkUzwylBo61r9Xa9NmgBI0swA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        .container {
+            width: 90%;
+            margin: 0 auto;
+        }
+
+        button {
+            cursor: pointer;
+        }
+    </style>
 </head>
+
 <body>
 
+    <header></header>
+
+    <main>
+
+        <section>
+            <h1 class="text-4xl text-center mt-10">Welcome to Laravel NewsPortal</h1>
+
+            <div class="container py-6">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo doloremque magni voluptate aliquam
+                    odio,
+                    soluta maxime nam recusandae iure beatae tempora quisquam voluptas ut sequi velit ipsum sit
+                    repellendus
+                    temporibus! Saepe perspiciatis totam harum, similique consectetur optio hic corrupti vero ab quas
+                    eius,
+                    beatae aliquam earum minima repellat maxime pariatur amet magni id, commodi minus iste dignissimos
+                    perferendis quia. Deleniti ex modi rem maxime nesciunt ipsum, fugiat ratione, accusamus voluptatum,
+                    obcaecati numquam sit iste! Similique sunt debitis itaque, dolore placeat ea aliquid maiores
+                    recusandae.
+                    Quibusdam quae ut enim iusto earum officia, cum atque aliquam ducimus corrupti fugiat, quia
+                    exercitationem
+                    eius.
+                </p>
+                <br>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo doloremque magni voluptate aliquam
+                    odio,
+                    soluta maxime nam recusandae iure beatae tempora quisquam voluptas ut sequi velit ipsum sit
+                    repellendus
+                    temporibus! Saepe perspiciatis totam harum, similique consectetur optio hic corrupti vero ab quas
+                    eius,
+                    beatae aliquam earum minima repellat maxime pariatur amet magni id, commodi minus iste dignissimos
+                    perferendis quia. Deleniti ex modi rem maxime nesciunt ipsum, fugiat ratione, accusamus voluptatum,
+                    obcaecati numquam sit iste! Similique sunt debitis itaque, dolore placeat ea aliquid maiores
+                    recusandae.
+                    Quibusdam quae ut enim iusto earum officia, cum atque aliquam ducimus corrupti fugiat, quia
+                    exercitationem
+                    eius.
+                </p>
+                <br>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo doloremque magni voluptate aliquam
+                    odio,
+                    soluta maxime nam recusandae iure beatae tempora quisquam voluptas ut sequi velit ipsum sit
+                    repellendus
+                    temporibus! Saepe perspiciatis totam harum, similique consectetur optio hic corrupti vero ab quas
+                    eius,
+                    beatae aliquam earum minima repellat maxime pariatur amet magni id, commodi minus iste dignissimos
+                    perferendis quia. Deleniti ex modi rem maxime nesciunt ipsum, fugiat ratione, accusamus voluptatum,
+                    obcaecati numquam sit iste! Similique sunt debitis itaque, dolore placeat ea aliquid maiores
+                    recusandae.
+                    Quibusdam quae ut enim iusto earum officia, cum atque aliquam ducimus corrupti fugiat, quia
+                    exercitationem
+                    eius.
+                </p>
+            </div>
+        </section>
+
+    </main>
+
+    <footer></footer>
+
 </body>
+
 </html>
